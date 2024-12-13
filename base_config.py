646d02eb3b3ecc97eab7db333eb2c166eeee48d5
pxxxl <3578126846@qq.com>
2024-12-13 22:26:01 +0800

    git commit -m "Stop tracking <file>"

--- base_config.py
+++
@@ -1,5 +0,0 @@
-proj_path = '/home/ethan/Project/Python/I3DV/U4Remote2'
-data_path = '/home/ethan/Project/Python/I3DV/dataset'
-record_path = '/home/ethan/Project/Python/I3DV/U4Remote2/record'
-
-default_config_path = '/home/ethan/Project/Python/I3DV/U4Remote2/configs/es_mask.json'
